7702befee32233f7e936a0ff95fccff47c24df81
0zturk-deniz <ddeniz.ozturkk@gmail.com>
2025-07-01 21:34:17 +0300

    adds postmapping to authservice

--- src/main/java/com/example/wit_tweet/controller/AuthController.java
+++ src/main/java/com/example/wit_tweet/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.example.wit_tweet.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @PostMapping("/register")
     public RegisterResponseDto register(@Validated @RequestBody RegisterRequestDto registerRequestDto){
         return authService.register(registerRequestDto);
     }
